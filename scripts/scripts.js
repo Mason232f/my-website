11552ca10381939b920e0e0d35e0271396337738
Mason232f <masonmeas83@gmail.com>
2026-09-23 12:42:30 -0400

    Create scripts.js

---
+++ scripts/scripts.js
@@ -0,0 +1,15 @@
+// This is a single line comment and is ignored by the browser
+
+/*
+This is a
+multi-line
+comment
+*/
+
+console.log('Hello World')       // Same as print() in python
+console.warn("Be Careful")
+console.error('This is an error message')
+
+// Write a script that logs your name, age,
+// and customized warning and error messages.
+
